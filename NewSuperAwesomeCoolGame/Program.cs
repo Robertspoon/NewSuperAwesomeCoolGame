cff4e41400fde9c3a531d7bf7b6333f7e375d988
Robertspoon <robertthemcjug@gmail.com>
2022-10-12 14:45:04 -0300

    Initial commit

---
+++ NewSuperAwesomeCoolGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewSuperAwesomeCoolGame
+{
+    internal class Program
+    {
+        static int health;
+        static int shield;
+        static int lives;
+        static int score;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<----------------------------->");
+            Console.WriteLine("NEW Super Awesome Cool Game!!!!");
+            Console.WriteLine("<----------------------------->");
+
+            health = 100;
+            shield = 100;
+            lives = 3;
+            score = 0;
+            
+        }
+        static void ShowHUD()
+        {
+
+        }
+    }
+}
