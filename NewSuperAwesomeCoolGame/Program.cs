94130ecf29c8c3fc3955dc7bf2c05a1a9fb2ee1f
Robertspoon <robertthemcjug@gmail.com>
2022-10-12 15:15:39 -0300

    Adding code and stuff

--- NewSuperAwesomeCoolGame/Program.cs
+++ NewSuperAwesomeCoolGame/Program.cs
@@ -12,22 +12,93 @@ namespace NewSuperAwesomeCoolGame
         static int shield;
         static int lives;
         static int score;
+        static int exp;
+        static int level;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("<----------------------------->");
-            Console.WriteLine("NEW Super Awesome Cool Game!!!!");
-            Console.WriteLine("<----------------------------->");
-
             health = 100;
             shield = 100;
             lives = 3;
             score = 0;
+            exp = 0;
+            level = 1;
+
+           
+           
+
+
+            Console.WriteLine("<----------------------------->");
+            Console.WriteLine("NEW Super Awesome Cool Game!!!!");
+            Console.WriteLine("<----------------------------->");
+            Console.ReadKey(true);
+            Console.WriteLine();
+            ShowHUD();
+
+            Console.ReadKey(true);
             
         }
         static void ShowHUD()
         {
+            string status;
+            status = "";
 
+            Console.WriteLine("<------------->");
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Experience points: " + exp);
+            Console.WriteLine("Lives: " + lives);
+
+            if ((health == 100) && (health >=76))
+            {
+                status = "FINE AND DANDY";
+            }
+            if ((health <=75 ) && (health >=51))
+            {
+                status = "JUST A FLESH WOUND";
+            }
+            if ((health <=50) && (health >= 26))
+            {
+                status = "NOT LOOKING TOO GOOD";
+            }
+            if ((health <=26) && (health >= 1))
+            {
+                status = "ONE FOOT IN THE GRAVE";
+            }
+            if (health == 0)
+            {
+                status = "DEAD";
+            }
+
+
+
+
+
+            Console.WriteLine("<------------->");
+        }
+        static void TakeDamage(int damage)
+        {
+            shield = shield - damage;
+            if (shield < 0)
+            {
+                shield = 0;
+            }
+            if (health < 0)
+            {
+                health = 0;
+            }
+            
+
+        }
+        static void GainExp(int gainEXP)
+        {
+            exp = exp + gainEXP;
+            if (exp >= 200)
+            {
+                exp = 0;
+            }
         }
     }
 }
